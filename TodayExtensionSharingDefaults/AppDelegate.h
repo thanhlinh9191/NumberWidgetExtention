2c69e2e83e6089106d9429e7d87e0eb86a637ec3
ThanhLinh <ntlinh@vn.isb.co.jp>
2014-10-10 16:51:41 +0700

    Initial Commit

---
+++ TodayExtensionSharingDefaults/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodayExtensionSharingDefaults
+//
+//  Created by ThanhLinh on 10/10/14.
+//  Copyright (c) 2014 ThanhLinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TodayExtensionSharingDefaults/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodayExtensionSharingDefaults
+//
+//  Created by ThanhLinh on 10/10/14.
+//  Copyright (c) 2014 ThanhLinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
